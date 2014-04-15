fdaa0c9904a594e3d5f472527706019712c563d7
Dominique Orban <dominique.orban@gmail.com>
2014-04-14 18:19:14 -0700

    Properly use format passed as argument.
    Thanks to Stefan Karpinski (http://goo.gl/sgg9lJ)

--- ampl_utils.jl
+++ ampl_utils.jl
@@ -1,9 +1,10 @@
 function print_array(x :: Array{Float64,1}; border=3, fmt="%8.1e")
+  @eval print_formatted(x) = @sprintf($fmt, x)
   n = length(x)
   b1, b2 = (n > 2 * border) ? (border, n-border+1) : (n, n+1)
   s = "["
   for k = 1 : b1
-    s *= @sprintf("%8.1e", x[k])
+    s *= print_formatted(x[k])
     s *= " "
   end
   if n > 2 * border
@@ -11,7 +12,7 @@ function print_array(x :: Array{Float64,1}; border=3, fmt="%8.1e")
   end
   for k = b2 : n
     s *= " "
-    s *= @sprintf("%8.1e", x[k])
+    s *= print_formatted(x[k])
   end
   s *= "]"
   println(s)
