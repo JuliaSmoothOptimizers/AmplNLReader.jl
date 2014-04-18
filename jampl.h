3dd0346ce4ab69ae6615ac04f5f1c793a3edc7ec
Dominique Orban <dominique.orban@gmail.com>
2014-04-18 01:00:43 -0700

    Beautify header.

--- jampl.h
+++ jampl.h
@@ -11,29 +11,33 @@
 
 void *jampl_init(char *stub);
 void jampl_finalize(void *asl);
+
 int jampl_objtype(void *asl);
-int jampl_nvar(void *asl);
-int jampl_ncon(void *asl);
-int jampl_nlc(void *asl);
-int jampl_nlnc(void *asl);
-int jampl_nnzj(void *asl);
-int jampl_nnzh(void *asl);
-int jampl_islp(void *asl);
-double *jampl_x0(void *asl);
-double *jampl_y0(void *asl);
+int jampl_nvar(   void *asl);
+int jampl_ncon(   void *asl);
+int jampl_nlc(    void *asl);
+int jampl_nlnc(   void *asl);
+int jampl_nnzj(   void *asl);
+int jampl_nnzh(   void *asl);
+int jampl_islp(   void *asl);
+
+double *jampl_x0(  void *asl);
+double *jampl_y0(  void *asl);
 double *jampl_lvar(void *asl);
 double *jampl_uvar(void *asl);
 double *jampl_lcon(void *asl);
 double *jampl_ucon(void *asl);
+
 void jampl_varscale(void *asl, double *s);
-double jampl_obj(void *asl, double *x);
-double *jampl_grad(void *asl, double *x);
-void jampl_lagscale(void *asl, double s);
+void jampl_lagscale(void *asl, double  s);
 void jampl_conscale(void *asl, double *s);
-double *jampl_cons(void *asl, double *x);
-double jampl_jcon(void *asl, double *x, int j);
+
+double  jampl_obj(     void *asl, double *x);
+double *jampl_grad(    void *asl, double *x);
+double *jampl_cons(    void *asl, double *x);
+double  jampl_jcon(    void *asl, double *x, int j);
 double *jampl_jcongrad(void *asl, double *x, int j);
-double *jampl_hprod(void *asl, double *x, double *y, double *v, double w);
+double *jampl_hprod(   void *asl, double *x, double *y, double *v, double w);
 double *jampl_ghjvprod(void *asl, double *x, double *g, double *v);
 
 // Functions that return a Julia-specific value.
