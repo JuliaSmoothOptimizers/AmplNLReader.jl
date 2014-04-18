2d8d6850a942e5ec1fc7f013b422b7da12853d98
Dominique Orban <dominique.orban@gmail.com>
2014-04-18 01:00:57 -0700

    Use multiple models in test.

--- test.jl
+++ test.jl
@@ -3,39 +3,44 @@
 include("ampl.jl")
 include("ampl_utils.jl")
 
-stub = "hs033.nl"
-nlp = AmplModel(stub)
+function exercise_ampl_model(nlp :: AmplModel)
+  @printf "problem %s\n" nlp.name
+  @printf "nvar = %d, ncon = %d\n" nlp.nvar nlp.ncon
+  @printf "lvar = "; print_array(nlp.lvar)
+  @printf "uvar = "; print_array(nlp.uvar)
+  @printf "lcon = "; print_array(nlp.lcon)
+  @printf "ucon = "; print_array(nlp.ucon)
+  @printf "x0 = "; print_array(nlp.x0)
+  @printf "y0 = "; print_array(nlp.y0)
 
-@printf "problem %s\n" nlp.name
-@printf "nvar = %d, ncon = %d\n" nlp.nvar nlp.ncon
-@printf "lvar = "; print_array(nlp.lvar)
-@printf "uvar = "; print_array(nlp.uvar)
-@printf "lcon = "; print_array(nlp.lcon)
-@printf "ucon = "; print_array(nlp.ucon)
-@printf "x0 = "; print_array(nlp.x0)
-@printf "y0 = "; print_array(nlp.y0)
+  f = obj( nlp, nlp.x0)
+  g = grad(nlp, nlp.x0)
+  c = cons(nlp, nlp.x0)
+  J = jac( nlp, nlp.x0)
+  H = hess(nlp, nlp.x0, y=ones(nlp.ncon,))
 
-f = obj( nlp, nlp.x0)
-g = grad(nlp, nlp.x0)
-c = cons(nlp, nlp.x0)
-J = jac( nlp, nlp.x0)
-H = hess(nlp, nlp.x0, y=ones(nlp.ncon,))
+  @printf "f(x0) = %f\n" f
+  @printf "∇f(x0) = "; print_array(g)
+  @printf "c(x0) = "; print_array(c)
+  for j = 1 : nlp.ncon
+    println("∇c_$j(x0) =")
+    println(jth_sparse_congrad(nlp, nlp.x0, j))
+  end
+  @printf "J(x0) = \n"; println(J)
+  @printf "∇²L(x0,y0) = \n"; println(H)
 
-@printf "f(x0) = %f\n" f
-@printf "∇f(x0) = "; print_array(g)
-@printf "c(x0) = "; print_array(c)
-for j = 1 : nlp.ncon
-  println("∇c_$j(x0) =")
-  println(jth_sparse_congrad(nlp, nlp.x0, j))
+  e = ones(nlp.nvar)
+  for j = 1 : nlp.ncon
+    print_formatted("∇²c_%d(x0) * e =", j);
+    print_array(jth_hprod(nlp, nlp.x0, e, j))
+  end
+
+  ghje = ghjvprod(nlp, nlp.x0, g, e)
+  @printf "(∇f(x0), ∇²c_j(x0) * e) = "; print_array(ghje)
 end
-@printf "J(x0) = \n"; println(J)
-@printf "∇²L(x0,y0) = \n"; println(H)
 
-e = ones(nlp.nvar)
-for j = 1 : nlp.ncon
-  print_formatted("∇²c_%d(x0) * e =", j);
-  print_array(jth_hprod(nlp, nlp.x0, e, j))
-end
+hs33 = AmplModel("hs033.nl")
+rosenbrock = AmplModel("rosenbr.nl")
+exercise_ampl_model(rosenbrock)
+exercise_ampl_model(hs33)
 
-ghje = ghjvprod(nlp, nlp.x0, g, e)
-@printf "(∇f(x0), ∇²c_j(x0) * e) = "; print_array(ghje)
