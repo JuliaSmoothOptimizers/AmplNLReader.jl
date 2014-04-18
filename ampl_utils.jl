f82ae404b6531b3689db2844372916fc2324471d
Dominique Orban <dominique.orban@gmail.com>
2014-04-17 17:57:34 -0700

    Make sprint_formatted available.

--- ampl_utils.jl
+++ ampl_utils.jl
@@ -1,10 +1,11 @@
-function print_array(x :: Array{Float64,1}; border=3, fmt="%8.1e")
-  @eval print_formatted(x) = @sprintf($fmt, x)
+sprint_formatted(fmt, args...) = @eval @sprintf($fmt, $(args...))
+
+function print_array(x :: Vector; border=3, fmt :: String = "%8.1e")
   n = length(x)
   b1, b2 = (n > 2 * border) ? (border, n-border+1) : (n, n+1)
   s = "["
   for k = 1 : b1
-    s *= print_formatted(x[k])
+    s *= sprint_formatted(fmt, x[k])
     s *= " "
   end
   if n > 2 * border
@@ -12,7 +13,7 @@ function print_array(x :: Array{Float64,1}; border=3, fmt="%8.1e")
   end
   for k = b2 : n
     s *= " "
-    s *= print_formatted(x[k])
+    s *= sprint_formatted(fmt, x[k])
   end
   s *= "]"
   println(s)
