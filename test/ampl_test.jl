5afcf9044835c9bc2693161b65aa3bf85a5c28b3
Dominique Orban <dominique.orban@gmail.com>
2015-02-14 15:17:50 -0500

    Exercise all methods.

--- test/ampl_test.jl
+++ test/ampl_test.jl
@@ -3,7 +3,12 @@
 using ampl
 
 function exercise_ampl_model(nlp :: AmplModel)
+  show(nlp)
   print(nlp)
+
+  # Perform dummy scaling.
+  varscale(nlp, ones(nlp.meta.nvar))
+  conscale(nlp, ones(nlp.meta.ncon))
 
   f = obj( nlp, nlp.meta.x0)
   g = grad(nlp, nlp.meta.x0)
@@ -15,15 +20,17 @@ function exercise_ampl_model(nlp :: AmplModel)
   @printf("∇f(x0) = "); display(g'); @printf("\n")
   @printf("c(x0) = ");  display(c'); @printf("\n")
   for j = 1 : nlp.meta.ncon
+    @printf("c_%d(x0) = %8.1e\n", j, jth_con(nlp, nlp.meta.x0, j));
     @printf("∇c_%d(x0) =", j)
     display(jth_congrad(nlp, nlp.meta.x0, j)); @printf("\n")
     @printf("sparse ∇c_%d(x0) =", j)
     display(jth_sparse_congrad(nlp, nlp.meta.x0, j)); @printf("\n")
   end
-  @printf "J(x0) = \n";      display(J); @printf("\n")
-  @printf "∇²L(x0,y0) = \n"; display(H); @printf("\n")
+  @printf("J(x0) = \n");      display(J); @printf("\n")
+  @printf("∇²L(x0,y0) = \n"); display(H); @printf("\n")
 
   e = ones(nlp.meta.nvar)
+  @printf("∇²L(x0,y0) * e = \n"); display(hprod(nlp, nlp.meta.x0, e)); @printf("\n")
   for j = 1 : nlp.meta.ncon
     Hje = jth_hprod(nlp, nlp.meta.x0, e, j)
     @printf("∇²c_%d(x0) * e = ", j); display(Hje'); @printf("\n")
@@ -31,6 +38,8 @@ function exercise_ampl_model(nlp :: AmplModel)
 
   ghje = ghjvprod(nlp, nlp.meta.x0, g, e)
   @printf "(∇f(x0), ∇²c_j(x0) * e) = "; display(ghje'); @printf("\n")
+
+  write_sol(nlp, "And the winner is...", rand(nlp.meta.nvar), rand(nlp.meta.ncon))
 end
 
 hs33 = AmplModel("hs033.nl")
