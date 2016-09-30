43b723a794361f447c83c352c8863e769a59f972
Dominique Orban <dominique.orban@gmail.com>
2016-09-30 04:47:14 -0400

    pour Homebrew bottle on osx

--- deps/build.jl
+++ deps/build.jl
@@ -4,21 +4,18 @@ using Compat
 @BinDeps.setup
 
 libasl = library_dependency("libasl", aliases=["libasl.3", "libasl.3.1.0"])
-libmp = library_dependency("libmp", aliases=["libmp.3", "libmp.3.1.0"])
 
-# Hopeless.
-# @osx_only begin
-#   using Homebrew
-#   provides(Homebrew.HB, "homebrew/science/asl", [libasl, libmp], os = :Darwin)
-#   push!(Libdl.DL_LOAD_PATH, joinpath(Homebrew.prefix("asl"), "lib"))
-# end
+@static if is_apple()
+  using Homebrew
+  provides(Homebrew.HB, "homebrew/science/asl", libasl, os = :Darwin)
+end
 
 # Uncomment when there is a deb for the ASL.
-# provides(AptGet, "libasl-dev", [libasl, libmp], os = :Linux)
+# provides(AptGet, "libasl-dev", libasl, os = :Linux)
 
 provides(Sources,
          URI("https://github.com/ampl/mp/archive/3.1.0.tar.gz"),
-         [libasl, libmp],
+         libasl,
          SHA="587c1a88f4c8f57bef95b58a8586956145417c8039f59b1758365ccc5a309ae9",
          unpacked_dir="mp-3.1.0")
 
@@ -32,7 +29,6 @@ provides(SimpleBuild,
             GetSources(libasl)
             CreateDirectory(builddir)
             (@build_steps begin
-          end), [libasl, libmp], os = :Unix)
               ChangeDirectory(builddir)
               (@build_steps begin
                 `cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_INSTALL_RPATH=$prefix/lib -DBUILD_SHARED_LIBS=True ..`
@@ -41,5 +37,6 @@ provides(SimpleBuild,
                 `make install`
               end)
             end)
+          end), libasl, os = :Unix)
 
 @BinDeps.install @compat Dict(:libasl => :libasl)
