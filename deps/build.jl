cca4a97d667ab47b00e1ac914250cdb3ad2a50a4
Dominique Orban <dominique.orban@gmail.com>
2018-09-12 13:58:49 -0400

    patch aslinterinterface

--- deps/build.jl
+++ deps/build.jl
@@ -31,6 +31,8 @@ provides(SimpleBuild,
             (@build_steps begin
               ChangeDirectory(builddir)
               (@build_steps begin
+               `wget https://gist.githubusercontent.com/dpo/b18b25879201ed986086c7c359fdcf99/raw/ac9df3fc1ff0ad6bbbb597b9673a6f1a2acee48c/a.rb`
+                pipeline(`cat a.rb`, `patch --verbose -p1 -d ..`)
                 `cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_INSTALL_RPATH=$prefix/lib -DBUILD_SHARED_LIBS=True ..`
                 `make all`
                 `make test`
