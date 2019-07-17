c35b0d4fc925a621402998ccbde56a8e1872598a
Dominique Orban <dominique.orban@gmail.com>
2019-07-17 12:03:46 -0400

    remove unused file

--- src/mpb_interface.jl
+++
@@ -1,6 +0,0 @@
-# An interface to pass a AmplModel to a MathProgBase solver
-
-using MathProgBase
-
-MathProgBase.features_available(::NLPModelEvaluator{AmplModel}) =
-  [:Grad, :Jac, :HessVec, :Hess]
