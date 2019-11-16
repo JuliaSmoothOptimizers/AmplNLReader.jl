aec9672d2477c3294c5234fb6daa4fb94cb72ab9
Dominique Orban <dominique.orban@gmail.com>
2019-11-16 16:44:02 -0500

    improve tests and coverage

--- test/ampl_test.jl
+++ test/ampl_test.jl
@@ -10,50 +10,102 @@ function exercise_ampl_model(nlp :: AmplModel)
   print(stdout, nlp)
 
   # Perform dummy scaling.
-  varscale(nlp, ones(nlp.meta.nvar))
-  conscale(nlp, ones(nlp.meta.ncon))
+  dummy_varscaling = ones(2 * nlp.meta.nvar)
+  dummy_conscaling = ones(2 * nlp.meta.ncon)
+  varscale(nlp, @view dummy_varscaling[1:2:end])  # to exercise AbstractArray input
+  conscale(nlp, @view dummy_conscaling[1:2:end])
+  lagscale(nlp, 1.0)
 
   f = obj( nlp, nlp.meta.x0)
   g = grad(nlp, nlp.meta.x0)
   c = cons(nlp, nlp.meta.x0)
   J = jac( nlp, nlp.meta.x0)
-  H = hess(nlp, nlp.meta.x0, ones(nlp.meta.ncon,))
 
-  @printf("f(x0) = %f\n", f)
-  @printf("∇f(x0) = "); display(g'); @printf("\n")
-  @printf("c(x0) = ");  display(c'); @printf("\n")
+  x = Vector{Cdouble}(undef, 2 * nlp.meta.nvar)
+  x[1:2:end] .= nlp.meta.x0
+  xview = @view x[1:2:end]
+
   for j = 1 : nlp.meta.ncon
-    @printf("c_%d(x0) = %8.1e\n", j, jth_con(nlp, nlp.meta.x0, j));
-    @printf("∇c_%d(x0) =", j)
-    display(jth_congrad(nlp, nlp.meta.x0, j)); @printf("\n")
-    @printf("sparse ∇c_%d(x0) =", j)
-    display(jth_sparse_congrad(nlp, nlp.meta.x0, j)); @printf("\n")
+    cj = jth_con(nlp, xview, j)
+    @test cj == c[j]
+    gj = jth_congrad(nlp, xview, j)
+    @test all(gj .== Vector{Float64}(J[j, :]))
+    sgj = jth_sparse_congrad(nlp, xview, j)
+    @test all(gj .== Vector{Float64}(sgj))
   end
-  @printf("J(x0) = \n");      display(J); @printf("\n")
-  @printf("∇²L(x0,y0) = \n"); display(H); @printf("\n")
+
+  jrows, jcols = jac_structure(nlp)
+  jvals = Vector{Float64}(undef, nlp.meta.nnzj)
+  jac_coord!(nlp, xview, jvals)
+  @test norm(sparse(jrows, jcols, jvals, nlp.meta.ncon, nlp.meta.nvar) - J) ≤ sqrt(eps()) * norm(J)
+
+  jvals2 = Vector{Float64}(undef, 2 * nlp.meta.nnzj)
+  jac_coord!(nlp, xview, @view jvals2[1:2:end])
+  @test all(jvals .== jvals2[1:2:end])
+
+  e2 = ones(nlp.meta.ncon)
+  H = hess(nlp, xview, e2)
+  hrows, hcols = hess_structure(nlp)
+  hvals = Vector{Float64}(undef, nlp.meta.nnzh)
+  hess_coord!(nlp, xview, e2, hvals)
+  @test norm(sparse(hrows, hcols, hvals, nlp.meta.nvar, nlp.meta.nvar) - H) ≤ sqrt(eps()) * norm(H)
+
+  hvals2 = Vector{Float64}(undef, 2 * nlp.meta.nnzh)
+  hess_coord!(nlp, xview, e2, @view hvals2[1:2:end])
+  @test all(hvals .== hvals2[1:2:end])
+
+  H = hess(nlp, xview)
+  hvals = Vector{Float64}(undef, nlp.meta.nnzh)
+  hess_coord!(nlp, xview, hvals)
+  @test norm(sparse(hrows, hcols, hvals, nlp.meta.nvar, nlp.meta.nvar) - H) ≤ sqrt(eps()) * norm(H)
+
+  hvals2 = Vector{Float64}(undef, 2 * nlp.meta.nnzh)
+  hess_coord!(nlp, xview, @view hvals2[1:2:end])
+  @test all(hvals .== hvals2[1:2:end])
 
   e = ones(nlp.meta.nvar)
-  je = jprod(nlp, nlp.meta.x0, e)
-  @printf("J(x0) * e = \n"); display(je); @printf("\n")
-  jte = jtprod(nlp, nlp.meta.x0, ones(nlp.meta.ncon))
-  @printf("J(x0)' * e = \n"); display(jte); @printf("\n")
-  je = rand(nlp.meta.ncon)
-  jprod!(nlp, nlp.meta.x0, e, je)
-  @printf("J(x0) * e = \n"); display(je); @printf("\n")
-  jte = rand(nlp.meta.nvar)
-  jtprod!(nlp, nlp.meta.x0, ones(nlp.meta.ncon), jte)
-  @printf("J(x0)' * e = \n"); display(jte); @printf("\n")
-  he = hprod(nlp, nlp.meta.x0, e)
-  @printf("∇²L(x0,y0) * e = \n"); display(he); @printf("\n")
-  hprod!(nlp, nlp.meta.x0, e, he)
-  @printf("∇²L(x0,y0) * e = \n"); display(he); @printf("\n")
+  je = jprod(nlp, xview, e)
+  @test norm(je - J * e) ≤ sqrt(eps()) * norm(je)
+
+  jte = jtprod(nlp, xview, e2)
+  @test norm(jte - J' * e2) ≤ sqrt(eps()) * norm(jte)
+
+  je2 = Vector{Float64}(undef, 2 * nlp.meta.ncon)
+  jprod!(nlp, xview, e, @view je2[1:2:end])
+  @test all(je .== je2[1:2:end])
+
+  jte2 = Vector{Float64}(undef, 2 * nlp.meta.nvar)
+  jtprod!(nlp, xview, e2, @view jte2[1:2:end])
+  @test all(jte .== jte2[1:2:end])
+
+  he = hprod(nlp, xview, e)
+  @test norm(he - Symmetric(hess(nlp, xview), :L) * e) ≤ sqrt(eps()) * norm(he)
+
+  he2 = Vector{Float64}(undef, 2 * nlp.meta.nvar)
+  hprod!(nlp, xview, e, @view he2[1:2:end])
+  @test all(he .== he2[1:2:end])
+
+  he2 = Vector{Float64}(undef, 2 * nlp.meta.nvar)
+  jth_hprod!(nlp, xview, e, 0, @view he2[1:2:end])  # same as objective hprod
+  @test all(he .== he2[1:2:end])
+
+  ghje = Vector{Float64}(undef, nlp.meta.ncon)
+  y = zeros(nlp.meta.ncon)
   for j = 1 : nlp.meta.ncon
-    Hje = jth_hprod(nlp, nlp.meta.x0, e, j)
-    @printf("∇²c_%d(x0) * e = ", j); display(Hje'); @printf("\n")
+    Hje = jth_hprod(nlp, xview, e, j)
+    y[j] = 1
+    H = hess(nlp, xview, y, obj_weight=0.0)
+    @test norm(Hje - H * e) ≤ sqrt(eps()) * norm(Hje)
+    y[j] = 0
+    ghje[j] = dot(g, Hje)
   end
 
-  ghje = ghjvprod(nlp, nlp.meta.x0, g, e)
-  @printf "(∇f(x0), ∇²c_j(x0) * e) = "; display(ghje'); @printf("\n")
+  ghje2 = ghjvprod(nlp, xview, g, e)
+  @test norm(ghje - ghje2) ≤ sqrt(eps()) * norm(ghje)
+
+  ghje = Vector{Float64}(undef, 2 * nlp.meta.ncon)
+  ghjvprod!(nlp, xview, g, e, @view ghje[1:2:end])
+  @test all(ghje2 .== ghje[1:2:end])
 
   write_sol(nlp, "And the winner is...", rand(nlp.meta.nvar), rand(nlp.meta.ncon))
   reset!(nlp)
@@ -69,6 +121,7 @@ exercise_ampl_model(hs9)
 amplmodel_finalize(hs33)
 amplmodel_finalize(rosenbrock)
 amplmodel_finalize(hs9)
+@test amplmodel_finalize(hs9) === nothing
 @test_throws AmplException obj(hs9, hs9.meta.x0)
 @test_throws AmplException AmplModel("this_file_does_not_exist")
 @test_throws AmplException AmplModel("this_file_does_not_exist.nl")
