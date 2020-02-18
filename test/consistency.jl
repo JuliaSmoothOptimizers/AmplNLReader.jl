0eacd585140d184140b14e56d14593c3b7fc4934
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2020-02-18 20:52:06 -0300

    Update NLPModels to 0.11 and version to 0.7.0

--- test/consistency.jl
+++ test/consistency.jl
@@ -1,13 +1,13 @@
 using NLPModels
 
-nlppath = joinpath(dirname(pathof(NLPModels)), "..", "test", "problems")
+nlppath = joinpath(dirname(pathof(NLPModels)), "..", "test")
 include(joinpath(nlppath, "consistency.jl"))
 testpath = dirname(@__FILE__)
 
 problems = [:brownden, :hs5, :hs6, :hs10, :hs11, :hs14]
 for problem in problems
   problem_s = string(problem)
-  include(joinpath(nlppath, "$problem_s.jl"))
+  include(joinpath(nlppath, "problems", "$problem_s.jl"))
 
   nlp_ampl = AmplModel(joinpath(testpath, "$problem_s.nl"), safe=true)
   nlp_autodiff = eval(Meta.parse("$(problem)_autodiff"))()
