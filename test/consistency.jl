868ca2006789ccf03fe339530c46772941664b19
Abel <abel.s.siqueira@gmail.com>
2021-03-14 22:43:43 -0300

    Use NLPModels 0.14 + NLPModelsTest and small refactoring

--- test/consistency.jl
+++ test/consistency.jl
@@ -1,18 +1,12 @@
-using NLPModels
+@testset "Consistency" begin
+  problems = ["BROWNDEN", "HS5", "HS6", "HS10", "HS11", "HS14"]
+  for problem in problems
+    @testset "Problem $problem" begin
+      nlp_ampl = AmplModel(joinpath("problems", lowercase(problem) * ".nl"), safe=true)
+      nlp_man = eval(Symbol(problem))()
+      nlps = [nlp_ampl, nlp_man]
 
-nlppath = joinpath(dirname(pathof(NLPModels)), "..", "test")
-include(joinpath(nlppath, "consistency.jl"))
-testpath = dirname(@__FILE__)
-
-problems = [:brownden, :hs5, :hs6, :hs10, :hs11, :hs14]
-for problem in problems
-  problem_s = string(problem)
-  include(joinpath(nlppath, "problems", "$problem_s.jl"))
-
-  nlp_ampl = AmplModel(joinpath(testpath, "$problem_s.nl"), safe=true)
-  nlp_autodiff = eval(Meta.parse("$(problem)_autodiff"))()
-  nlps = [nlp_ampl, nlp_autodiff]
-
-  @printf("Checking problem %-15s%12s\t", problem_s, "")
-  consistent_nlps(nlps)
+      consistent_nlps(nlps)
+    end
+  end
 end
