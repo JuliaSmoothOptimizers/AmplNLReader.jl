4a692f48e483f7a2108a6c75b5f83667d57b4543
Dominique <dominique.orban@gmail.com>
2022-07-07 15:43:16 -0400

    use push_preview = true in deploydocs
    With this change, a new workflow named "documenter/deploy" appears in the list of checks when the docs have finished building.
    Clicking on "details" takes you to the docs resulting from the pull request.

--- docs/make.jl
+++ docs/make.jl
@@ -13,4 +13,8 @@ makedocs(
   pages = Any["Home" => "index.md", "API" => "api.md", "Reference" => "reference.md"],
 )
 
-deploydocs(repo = "github.com/JuliaSmoothOptimizers/AmplNLReader.jl.git", devbranch = "main")
+deploydocs(
+  repo = "github.com/JuliaSmoothOptimizers/AmplNLReader.jl.git",
+  push_preview = true,
+  devbranch = "main",
+)
