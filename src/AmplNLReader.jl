2c08cde7d1594d8664b16c0d3fcdbf5f08324617
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2024-10-04 11:15:48 -0500

    Use the macro ccall (#148)
    * Use the macro ccall
    
    * Update Project/toml
    
    * Update docs/Project.toml

--- src/AmplNLReader.jl
+++ src/AmplNLReader.jl
@@ -5,10 +5,9 @@ module AmplNLReader
 using NLPModels
 using SparseArrays
 
-using Libdl
-using ASL_jll
+import ASL_jll
 
-const libasl = joinpath(dirname(ASL_jll.libasl_path), "libasl." * dlext)
+const libasl = ASL_jll.libasl
 
 include("ampl_meta.jl")
 include("ampl_model.jl")
